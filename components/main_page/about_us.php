7e1784eb9cd239149f60a35393ad96dd14cccea3
goldipl <mgodlewski92@gmail.com>
2025-06-19 19:52:18 +0200

    Implement code changes to enhance functionality and improve performance

--- components/main_page/about_us.php
+++ components/main_page/about_us.php
@@ -1,15 +1,39 @@
 <section id="about_us">
-    <div class="container about-us-box">
-        <div class="about-us-box__img">
-            <img src="./assets/img/main_page/about_us/bulb.jpg" alt="obraz" width="378" height="408">
+  <div class="container d-flex flex-row align-items-center justify-content-center">
+    <div class="d-flex flex-column py-4 mp-titles align-items-center justify-content-center">
+      <div class="d-flex flex-row mp-superscription">
+        <h3>Co nas wyróżnia</h3>
+      </div>
+      <div class="d-flex flex-row mp-title">
+        <h2>Dlaczego my</h2>
+      </div>
+      <div class="title-red-line"></div>
+    </div>
+  </div>
+  <div class="container about-us-box">
+    <div class="about-us-box__text">
+        <div class="about-text-slot">
+            <div class="about-text-slot__icon">
+                <img src="./assets/icons/main_page/about_us/printer.svg" alt="ikona jakości" width="32" height="32">
+            </div>
+            <div class="about-text-slot__text">
+                <h4>Własne zaplecze produkcyjne</h4>
+                <p>(co pozwala nam na w pełni kompleksową i profesjonalną obsługę w najkrótszym możliwym czasie. Jesteśmy w stanie błyskawicznie zareagować na wszelkie zmiany, nanieść ewentualne poprawki i dochować należytej  staranności oraz jakości znakowanych przez nas produktów).</p>
+            </div>
         </div>
-        <div class="about-us-box__text">
-            <h2>Dlaczego my?</h2>
-            <h3>Gadżety reklamowe - to proste!</h3>
-            <p><strong>Oferujemy najciekawszy wybór gadżetów reklamowych oraz darmowe narzędzia  wspierające sprzedaż dla agencji reklamowych 
-            - sprawdź co możesz zyskać!</strong></p>
-            <p>Jesteś przedstawicielem agencji reklamowej? Szukasz dostawcy gadżetów  reklamowych, który nie tylko zrealizuje Twoje zamówienia kompleksowo,  ale też zapewni wsparcie w codziennej pracy?</p>
-            <p>Poznaj nas! Jesteśmy jednym z największych importerów gadżetów promocyjnych w Polsce. <strong>Współpracujemy tylko i wyłącznie z agencjami reklamowymi i pośrednikami dzięki czemu  możemy zapewnić najwyższą jakość usług na całym etapie realizacji  zlecenia.</strong> Własna drukarnia i zespół specjalistów pozwalają nam  zapewnić ekspresowe wykonanie znakowania na gadżetach. Mamy kreatywne  podejście do nadruków i właśnie tym podejściem pragniemy dzielić się z  naszymi Klientami.</p>
+        <div class="about-text-slot">
+            <div class="about-text-slot__icon">
+                <img src="./assets/icons/main_page/about_us/studio.svg" alt="ikona jakości" width="32" height="32">
+            </div>
+            <div class="about-text-slot__text">
+                <h4>Własne studio DTP i kreacji</h4>
+                <p>(które tworzy niestandardowe rozwiązania i projekty pod  potrzeby naszych klientów oraz pełne wizualizacje znakowania wybranych  przedmiotów).</p>
+            </div>
         </div>
     </div>
+    <div class="about-us-box__img">
+      <img src="./assets/img/main_page/about_us/office-worker.jpg" alt="pracownik biurowy" width="601" height="478">
+    </div>
+
+  </div>
 </section>
