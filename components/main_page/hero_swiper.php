4e66c2d179d54b365473d56af2f2663c30e17891
goldipl <mgodlewski92@gmail.com>
2025-06-19 14:33:00 +0200

    Refactor Swiper styles: Adjust font sizes, colors, and pagination layout
    - Decreased font sizes for h1 and h2 elements for better readability.
    - Updated color for headings to use the main color instead of the secondary color.
    - Added max-width to headings for improved layout.
    - Modified pagination styles for better alignment and spacing.
    - Changed bullet sizes and colors for pagination to enhance visibility.
    - Adjusted margins for paragraph elements to improve spacing.
    - Introduced styles for color buttons within the swiper component.

--- components/main_page/hero_swiper.php
+++ components/main_page/hero_swiper.php
@@ -1,113 +1,129 @@
 <section id="swiper" class="container main-page-swiper">
-    <!-- Slider main container -->
-    <div class="swiper desktop">
-        <!-- Additional required wrapper -->
-        <div class="swiper-wrapper">
-            <!-- Slides -->
-            <div class="swiper-slide">
-                <a href="#our_products_container">
-                    <img src="./assets/img/main_page/hero_swiper/hero_swiper02.jpg" alt="banner" width="1200" height="383">
-                </a>
-                <div class="hero_content--content">
-                    <h3>Personalizowane gadżety reklamowe!</h3>
-                    <h1><span>Twoja marka -</span> Twój styl</h1>
-                    <p>Realizujemy wyjątkowe gadżety reklamowe, które podkreślą charakter Twojej marki i przyciągną uwagę klientów. Oferujemy pełną personalizację, wysoką jakość wykonania oraz terminową realizację zamówień.</p>
-                    <a class="color-btn" href="#our_products_container">Sprawdź<img src="./assets/icons/common/buttons/button_white_arrow.svg" alt="arrow icon" width="18" height="18"></a>
-                </div>
-            </div>
-            <div class="swiper-slide">
-                <a href="#our_products_container">
-                    <img src="./assets/img/main_page/hero_swiper/hero_swiper02.jpg" alt="banner" width="1200" height="383">
-                </a>
-                <div class="hero_content--content">
-                    <h3>Personalizowane gadżety reklamowe!</h3>
-                    <h1><span>Twoja marka -</span> Twój styl</h1>
-                    <p>Realizujemy wyjątkowe gadżety reklamowe, które podkreślą charakter Twojej marki i przyciągną uwagę klientów. Oferujemy pełną personalizację, wysoką jakość wykonania oraz terminową realizację zamówień.</p>
-                    <a class="color-btn" href="#our_products_container">Sprawdź<img src="./assets/icons/common/buttons/button_white_arrow.svg" alt="arrow icon" width="18" height="18"></a>
-                </div>
-            </div>
-            <div class="swiper-slide">
-                <a href="#our_products_container">
-                    <img src="./assets/img/main_page/hero_swiper/hero_swiper02.jpg" alt="banner" width="1200" height="383">
-                </a>
-                <div class="hero_content--content">
-                    <h3>Personalizowane gadżety reklamowe!</h3>
-                    <h1><span>Twoja marka -</span> Twój styl</h1>
-                    <p>Realizujemy wyjątkowe gadżety reklamowe, które podkreślą charakter Twojej marki i przyciągną uwagę klientów. Oferujemy pełną personalizację, wysoką jakość wykonania oraz terminową realizację zamówień.</p>
-                    <a class="color-btn" href="#our_products_container">Sprawdź<img src="./assets/icons/common/buttons/button_white_arrow.svg" alt="arrow icon" width="18" height="18"></a>
-                </div>
-            </div>
-            <div class="swiper-slide">
-                <a href="#our_products_container">
-                    <img src="./assets/img/main_page/hero_swiper/hero_swiper02.jpg" alt="banner" width="1200" height="383">
-                </a>
-                <div class="hero_content--content">
-                    <h3>Personalizowane gadżety reklamowe!</h3>
-                    <h1><span>Twoja marka -</span> Twój styl</h1>
-                    <p>Realizujemy wyjątkowe gadżety reklamowe, które podkreślą charakter Twojej marki i przyciągną uwagę klientów. Oferujemy pełną personalizację, wysoką jakość wykonania oraz terminową realizację zamówień.</p>
-                    <a class="color-btn" href="#our_products_container">Sprawdź<img src="./assets/icons/common/buttons/button_white_arrow.svg" alt="arrow icon" width="18" height="18"></a>
-                </div>
-            </div>
+  <!-- Slider main container -->
+  <div class="swiper desktop">
+    <!-- Additional required wrapper -->
+    <div class="swiper-wrapper">
+      <!-- Slides -->
+      <div class="swiper-slide">
+        <a href="#our_products_container">
+          <img src="./assets/img/main_page/hero_swiper/hero_swiper02.jpg" alt="banner" width="1200" height="383">
+        </a>
+        <div class="hero_content--content">
+          <h1>
+            <span>Spersonalizowane prezenty,</span> które robią wrażenie!
+          </h1>
+          <p>Odkryj szeroki wybór ekskluzywnych upominków reklamowych. Wyróżnij swoją markę dzięki wysokiej jakości gadżetom z nadrukiem – stylowe, praktyczne i idealne na każdą okazję!</p>
+          <a class="color-btn" href="#our_products_container">Sprawdź <img src="./assets/icons/common/buttons/button_white_arrow.svg" alt="arrow icon" width="18" height="18">
+          </a>
         </div>
-        <!-- If we need pagination -->
-        <div class="swiper-pagination"></div>
-        <!-- If we need navigation buttons -->
-        <div class="swiper-button-prev"></div>
-        <div class="swiper-button-next"></div>
-    </div>
-    <div class="swiper mobile">
-        <!-- Additional required wrapper -->
-        <div class="swiper-wrapper">
-            <!-- Slides -->
-            <div class="swiper-slide">
-                <a href="#our_products_container">
-                    <img src="./assets/img/main_page/hero_swiper/hero_swiper_mobile02.jpg" alt="banner" width="491" height="405">
-                </a>
-                <div class="hero_content--content">
-                    <h3>Personalizowane gadżety reklamowe!</h3>
-                    <h1><span>Twoja marka -</span> Twój styl</h1>
-                    <p>Realizujemy wyjątkowe gadżety reklamowe, które podkreślą charakter Twojej marki i przyciągną uwagę klientów. Oferujemy pełną personalizację, wysoką jakość wykonania oraz terminową realizację zamówień.</p>
-                    <a class="color-btn" href="#our_products_container">Sprawdź<img src="./assets/icons/common/buttons/button_white_arrow.svg" alt="arrow icon" width="18" height="18"></a>
-                </div>
-            </div>
-            <div class="swiper-slide">
-                <a href="#our_products_container">
-                    <img src="./assets/img/main_page/hero_swiper/hero_swiper_mobile02.jpg" alt="banner" width="491" height="405">
-                </a>
-                <div class="hero_content--content">
-                    <h3>Personalizowane gadżety reklamowe!</h3>
-                    <h1><span>Twoja marka -</span> Twój styl</h1>
-                    <p>Realizujemy wyjątkowe gadżety reklamowe, które podkreślą charakter Twojej marki i przyciągną uwagę klientów. Oferujemy pełną personalizację, wysoką jakość wykonania oraz terminową realizację zamówień.</p>
-                    <a class="color-btn" href="#our_products_container">Sprawdź<img src="./assets/icons/common/buttons/button_white_arrow.svg" alt="arrow icon" width="18" height="18"></a>
-                </div>
-            </div>
-            <div class="swiper-slide">
-                <a href="#our_products_container">
-                    <img src="./assets/img/main_page/hero_swiper/hero_swiper_mobile02.jpg" alt="banner" width="491" height="405">
-                </a>
-                <div class="hero_content--content">
-                    <h3>Personalizowane gadżety reklamowe!</h3>
-                    <h1><span>Twoja marka -</span> Twój styl</h1>
-                    <p>Realizujemy wyjątkowe gadżety reklamowe, które podkreślą charakter Twojej marki i przyciągną uwagę klientów. Oferujemy pełną personalizację, wysoką jakość wykonania oraz terminową realizację zamówień.</p>
-                    <a class="color-btn" href="#our_products_container">Sprawdź<img src="./assets/icons/common/buttons/button_white_arrow.svg" alt="arrow icon" width="18" height="18"></a>
-                </div>
-            </div>
-            <div class="swiper-slide">
-                <a href="#our_products_container">
-                    <img src="./assets/img/main_page/hero_swiper/hero_swiper_mobile02.jpg" alt="banner" width="491" height="405">
-                </a>
-                <div class="hero_content--content">
-                    <h3>Personalizowane gadżety reklamowe!</h3>
-                    <h1><span>Twoja marka -</span> Twój styl</h1>
-                    <p>Realizujemy wyjątkowe gadżety reklamowe, które podkreślą charakter Twojej marki i przyciągną uwagę klientów. Oferujemy pełną personalizację, wysoką jakość wykonania oraz terminową realizację zamówień.</p>
-                    <a class="color-btn" href="#our_products_container">Sprawdź<img src="./assets/icons/common/buttons/button_white_arrow.svg" alt="arrow icon" width="18" height="18"></a>
-                </div>
-            </div>
+      </div>
+      <div class="swiper-slide">
+        <a href="#our_products_container">
+          <img src="./assets/img/main_page/hero_swiper/hero_swiper02.jpg" alt="banner" width="1200" height="383">
+        </a>
+        <div class="hero_content--content">
+          <h1>
+            <span>Spersonalizowane prezenty,</span> które robią wrażenie!
+          </h1>
+          <p>Odkryj szeroki wybór ekskluzywnych upominków reklamowych. Wyróżnij swoją markę dzięki wysokiej jakości gadżetom z nadrukiem – stylowe, praktyczne i idealne na każdą okazję!</p>
+          <a class="color-btn" href="#our_products_container">Sprawdź <img src="./assets/icons/common/buttons/button_white_arrow.svg" alt="arrow icon" width="18" height="18">
+          </a>
         </div>
-        <!-- If we need pagination -->
-        <div class="swiper-pagination"></div>
-        <!-- If we need navigation buttons -->
-        <div class="swiper-button-prev"></div>
-        <div class="swiper-button-next"></div>
+      </div>
+      <div class="swiper-slide">
+        <a href="#our_products_container">
+          <img src="./assets/img/main_page/hero_swiper/hero_swiper02.jpg" alt="banner" width="1200" height="383">
+        </a>
+        <div class="hero_content--content">
+          <h1>
+            <span>Spersonalizowane prezenty,</span> które robią wrażenie!
+          </h1>
+          <p>Odkryj szeroki wybór ekskluzywnych upominków reklamowych. Wyróżnij swoją markę dzięki wysokiej jakości gadżetom z nadrukiem – stylowe, praktyczne i idealne na każdą okazję!</p>
+          <a class="color-btn" href="#our_products_container">Sprawdź <img src="./assets/icons/common/buttons/button_white_arrow.svg" alt="arrow icon" width="18" height="18">
+          </a>
+        </div>
+      </div>
+      <div class="swiper-slide">
+        <a href="#our_products_container">
+          <img src="./assets/img/main_page/hero_swiper/hero_swiper02.jpg" alt="banner" width="1200" height="383">
+        </a>
+        <div class="hero_content--content">
+          <h1>
+            <span>Spersonalizowane prezenty,</span> które robią wrażenie!
+          </h1>
+          <p>Odkryj szeroki wybór ekskluzywnych upominków reklamowych. Wyróżnij swoją markę dzięki wysokiej jakości gadżetom z nadrukiem – stylowe, praktyczne i idealne na każdą okazję!</p>
+          <a class="color-btn" href="#our_products_container">Sprawdź <img src="./assets/icons/common/buttons/button_white_arrow.svg" alt="arrow icon" width="18" height="18">
+          </a>
+        </div>
+      </div>
     </div>
+    <!-- If we need pagination -->
+    <div class="swiper-pagination"></div>
+    <!-- If we need navigation buttons -->
+    <div class="swiper-button-prev"></div>
+    <div class="swiper-button-next"></div>
+  </div>
+  <div class="swiper mobile">
+    <!-- Additional required wrapper -->
+    <div class="swiper-wrapper">
+      <!-- Slides -->
+      <div class="swiper-slide">
+        <a href="#our_products_container">
+          <img src="./assets/img/main_page/hero_swiper/hero_swiper_mobile02.jpg" alt="banner" width="491" height="405">
+        </a>
+        <div class="hero_content--content">
+          <h1>
+            <span>Spersonalizowane prezenty,</span> które robią wrażenie!
+          </h1>
+          <p>Odkryj szeroki wybór ekskluzywnych upominków reklamowych. Wyróżnij swoją markę dzięki wysokiej jakości gadżetom z nadrukiem – stylowe, praktyczne i idealne na każdą okazję!</p>
+          <a class="color-btn" href="#our_products_container">Sprawdź <img src="./assets/icons/common/buttons/button_white_arrow.svg" alt="arrow icon" width="18" height="18">
+          </a>
+        </div>
+      </div>
+      <div class="swiper-slide">
+        <a href="#our_products_container">
+          <img src="./assets/img/main_page/hero_swiper/hero_swiper_mobile02.jpg" alt="banner" width="491" height="405">
+        </a>
+        <div class="hero_content--content">
+          <h1>
+            <span>Spersonalizowane prezenty,</span> które robią wrażenie!
+          </h1>
+          <p>Odkryj szeroki wybór ekskluzywnych upominków reklamowych. Wyróżnij swoją markę dzięki wysokiej jakości gadżetom z nadrukiem – stylowe, praktyczne i idealne na każdą okazję!</p>
+          <a class="color-btn" href="#our_products_container">Sprawdź <img src="./assets/icons/common/buttons/button_white_arrow.svg" alt="arrow icon" width="18" height="18">
+          </a>
+        </div>
+      </div>
+      <div class="swiper-slide">
+        <a href="#our_products_container">
+          <img src="./assets/img/main_page/hero_swiper/hero_swiper_mobile02.jpg" alt="banner" width="491" height="405">
+        </a>
+        <div class="hero_content--content">
+          <h1>
+            <span>Spersonalizowane prezenty,</span> które robią wrażenie!
+          </h1>
+          <p>Odkryj szeroki wybór ekskluzywnych upominków reklamowych. Wyróżnij swoją markę dzięki wysokiej jakości gadżetom z nadrukiem – stylowe, praktyczne i idealne na każdą okazję!</p>
+          <a class="color-btn" href="#our_products_container">Sprawdź <img src="./assets/icons/common/buttons/button_white_arrow.svg" alt="arrow icon" width="18" height="18">
+          </a>
+        </div>
+      </div>
+      <div class="swiper-slide">
+        <a href="#our_products_container">
+          <img src="./assets/img/main_page/hero_swiper/hero_swiper_mobile02.jpg" alt="banner" width="491" height="405">
+        </a>
+        <div class="hero_content--content">
+          <h1>
+            <span>Spersonalizowane prezenty,</span> które robią wrażenie!
+          </h1>
+          <p>Odkryj szeroki wybór ekskluzywnych upominków reklamowych. Wyróżnij swoją markę dzięki wysokiej jakości gadżetom z nadrukiem – stylowe, praktyczne i idealne na każdą okazję!</p>
+          <a class="color-btn" href="#our_products_container">Sprawdź <img src="./assets/icons/common/buttons/button_white_arrow.svg" alt="arrow icon" width="18" height="18">
+          </a>
+        </div>
+      </div>
+    </div>
+    <!-- If we need pagination -->
+    <div class="swiper-pagination"></div>
+    <!-- If we need navigation buttons -->
+    <div class="swiper-button-prev"></div>
+    <div class="swiper-button-next"></div>
+  </div>
 </section>
