5aa78d494b103197d363ccd2fe02827ca180e882
goldipl <mgodlewski92@gmail.com>
2025-06-19 21:08:36 +0200

    Refactor hover effects and image filters in SCSS files
    - Updated hover styles in _info_section.scss to change text color and image filter.
    - Changed image filter in _titles.scss for consistency across components.
    - Modified hover image filters in _product_card_tabs.scss for improved visual effects.

--- components/main_page/info_section.php
+++ components/main_page/info_section.php
@@ -3,47 +3,47 @@
         <div class="info-section-box">
             <a href="" class="info-section-box__slot">
                 <div class="info_section__top">
-                    <img src="./assets/icons/main_page/info_section/import.svg" alt="Import" width="57" height="50">
-                    <p>Import</p>
-                </div>
-                <div class="info_section__bottom">
-                    <p>Zrealizujemy Twoje pomysły. Stwórz z nami gadżety, jakich nie ma nikt inny!</p>
-                </div>
-            </a>
-            <a href="" class="info-section-box__slot">
-                <div class="info_section__top">
-                    <img src="./assets/icons/main_page/info_section/shop.svg" alt="SKLEP LUB KATALOG NO-NAME" width="57" height="50">
-                    <p>SKLEP LUB KATALOG NO-NAME</p>
-                </div>
-                <div class="info_section__bottom">
-                    <p>Zbuduj własny sklep internetowy lub katalog no-name za darmo!</p>
-                </div>
-            </a>
-            <a href="" class="info-section-box__slot">
-                <div class="info_section__top">
-                    <img src="./assets/icons/main_page/info_section/print.svg" alt="Drukarnia gadżetowa" width="57" height="50">
-                    <p>Drukarnia gadżetowa</p>
-                </div>
-                <div class="info_section__bottom">
-                    <p>Zamawiaj produkty ze znakowaniem i drukuj na towarze powierzonym</p>
-                </div>
-            </a>
-            <a href="" class="info-section-box__slot">
-                <div class="info_section__top">
-                    <img src="./assets/icons/main_page/info_section/delivery.svg" alt="Szybka realizacja" width="57" height="50">
+                    <img src="./assets/icons/main_page/info_section/fast-realization.svg" alt="Szybka realizacja" width="57" height="50">
                     <p>Szybka realizacja</p>
                 </div>
                 <div class="info_section__bottom">
-                    <p>Nie trać czasu! Zrealizujemy Twoje zamówienia szybko i sprawnie</p>
+                    <p>Szybka realizacja zamówień – na czas, zawsze!</p>
                 </div>
             </a>
             <a href="" class="info-section-box__slot">
                 <div class="info_section__top">
-                    <img src="./assets/icons/main_page/info_section/support.svg" alt="Serwis klienta" width="57" height="50">
+                    <img src="./assets/icons/main_page/info_section/wide-offert.svg" alt="Szeroki asortyment" width="57" height="50">
+                    <p>Szeroki asortyment</p>
+                </div>
+                <div class="info_section__bottom">
+                    <p>Bogaty asortyment upominków reklamowych!</p>
+                </div>
+            </a>
+            <a href="" class="info-section-box__slot">
+                <div class="info_section__top">
+                    <img src="./assets/icons/main_page/info_section/low-prices.svg" alt="Niskie ceny" width="57" height="50">
+                    <p>Niskie ceny</p>
+                </div>
+                <div class="info_section__bottom">
+                    <p>Atrakcyjne ceny, które Cię zaskoczą!</p>
+                </div>
+            </a>
+            <a href="" class="info-section-box__slot">
+                <div class="info_section__top">
+                    <img src="./assets/icons/main_page/info_section/high-experience.svg" alt="Duże doświadczenie" width="57" height="50">
+                    <p>Duże doświadczenie</p>
+                </div>
+                <div class="info_section__bottom">
+                    <p>Ponad 10 lat doświadczenia w branży!</p>
+                </div>
+            </a>
+            <a href="" class="info-section-box__slot">
+                <div class="info_section__top">
+                    <img src="./assets/icons/main_page/info_section/client-service.svg" alt="Serwis klienta" width="57" height="50">
                     <p>Serwis klienta</p>
                 </div>
                 <div class="info_section__bottom">
-                    <p>Masz pytania? Skontaktuj się z naszymi doświadczonymi specjalistami</p>
+                    <p>Profesjonalna obsługa klienta na każdym etapie!</p>
                 </div>
             </a>
         </div>
