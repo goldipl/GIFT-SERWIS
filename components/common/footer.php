ee5e3df9c372daa67d2a05dd1ba35db3fe37f059
goldipl <mgodlewski92@gmail.com>
2025-06-20 10:48:26 +0200

    Add responsive styles for swiper navigation buttons and new footer icons
    - Updated _swiper.scss to include responsive width and height for navigation buttons on screens smaller than 991px.
    - Added new SVG icons for footer: blue-envelope.svg, blue-location.svg, and blue-telephone.svg.

--- components/common/footer.php
+++ components/common/footer.php
@@ -1,63 +1,81 @@
 <div class="footer__container">
-    <div class="footer__container__top container d-flex flex-row justify-content-between">
-        <div class="d-flex flex-column col-4 first-column">
-            <div class="footer-logo">
-                <img src="./assets/icons/common/footer/logo-footer.svg" alt="GiftSerwis - Dystrybutor gadżetów reklamowych" width="301" height="86">
-            </div>
-            <div class="footer-text">
-                <p>Bardzo szeroki zakres dostarczanych przez nas artykułów począwszy od  porcelany, poprzez tekstylia, produkty codziennego użytku, artykuły  piśmienne, na elektronice kończąc, daje możliwość współpracy z  odbiorcami praktycznie każdej branży.</p>
-            </div>
-        </div>
-        <div class="d-flex flex-column align-items-center col-2 second-column">
-            <div class="footer-menu-box d-flex flex-column">
-                <h2>Menu</h2>
-                <a href="#">OFERTA NO1</a>
-                <a href="#">Marki</a>
-                <a href="#">Nowości</a>
-                <a href="#">Tematyczne</a>
-                <a href="#">Promocje</a>
-                <a href="#">Import</a>
-                <a href="#">Katalogi</a>
-                <a href="#">Znakowanie</a>
-                <a href="#">Kontakt</a>
-            </div>
-        </div>
-        <div class="d-flex flex-column align-items-center col-2 third-column">
-            <div class="footer-socials-box d-flex flex-column">
-                <h2>Nasze marki</h2>
-                <a href="#">MoLu</a>
-                <a href="#">Victorinox</a>
-                <a href="#">Wenger</a>
-                <a href="#">Schwarzwolf</a>
-                <a href="#">Pierre Cardin</a>
-                <a href="#">Silicon Power</a>
-            </div>
-        </div>
-        <div class="d-flex flex-column align-items-center col-2 fourth-column">
-            <div class="footer-contacts-box d-flex flex-column">
-                <h2>Pomoc</h2>
-                <a href="#">Polityka prywatności </a>
-                <a href="#">Regulamin sklepu</a>
-                <a href="#">Najczęściej zadawane pytania</a>
-            </div>
-        </div>
-        <div class="d-flex flex-column align-items-center col-2 fifth-column">
-            <div class="footer-contacts-box d-flex flex-column">
-                <h2>Kontakt</h2>
-                <div class="footer-contact-box">
-                    <p><img src="./assets/icons/common/footer/contact.svg" alt="telefon" width="18" height="18"> <a class="gtm-phone" href="tel:+48 713 152 330">+48 713 152 330</a></p>
-                </div>
-                <div class="footer-contact-box">
-                    <p><img src="./assets/icons/common/footer/letter.svg" alt="e-mail" width="18" height="18"> <a class="gtm-mail" href="mailto:biuro@GiftSerwis.pl">biuro@GiftSerwis.pl</a></p>
-                </div>
-                <div class="footer-contact-box">
-                    <p><img src="./assets/icons/common/footer/location.svg" alt="adres" width="18" height="18"></p>
-                    <div class="address-footer d-flex flex-column"><span>ul. Wrocławska 41 Byków</span> <span>55-095 Mirków</span></div>
-                </div>
-            </div>
-        </div>
+  <div class="footer__container__top container d-flex flex-row justify-content-between">
+    <div class="d-flex flex-column col-3 first-column">
+      <div class="footer-logo">
+        <img src="./assets/icons/common/footer/logo-footer.svg" alt="GiftSerwis - Dystrybutor gadżetów reklamowych" width="301" height="86">
+      </div>
+      <div class="footer-text">
+        <p>Bardzo szeroki zakres dostarczanych przez nas artykułów począwszy od porcelany, poprzez tekstylia, produkty codziennego użytku, artykuły piśmienne, na elektronice kończąc, daje możliwość współpracy z odbiorcami praktycznie każdej branży.</p>
+      </div>
     </div>
-    <div class="footer__container__bottom d-flex justify-content-center align-items-center">
-        <p>All rights reserved. Copyright by GIFTSERWIS <span></span>.</p><p class="contip">Realizacja <a href="https://www.contip.net/"><img src="./assets/img/common/footer/contip-logo.png" height="24" alt="Contip logo"></a></p>
+    <div class="d-flex flex-column align-items-start col-3 second-column">
+      <div class="footer-contacts-box d-flex flex-column">
+        <h2>Kontakt biuro</h2>
+        <div class="footer-contact-box">
+          <div class="contact-icon">
+            <img src="./assets/icons/common/footer/blue-telephone.svg" alt="telefon" width="18" height="18">
+          </div>
+          <a class="gtm-phone" href="tel:91 433 97 11">91 433 97 11</a>
+        </div>
+        <div class="footer-contact-box">
+          <div class="contact-icon">
+            <img src="./assets/icons/common/footer/blue-envelope.svg" alt="e-mail" width="18" height="18">
+          </div>
+          <a class="gtm-mail" href="mailto:gift@giftserwis.com.pl">gift@giftserwis.com.pl</a>
+        </div>
+        <div class="footer-contact-box">
+          <div class="contact-icon">
+            <img src="./assets/icons/common/footer/blue-location.svg" alt="adres" width="18" height="18">
+          </div>
+          <div class="address-footer d-flex flex-column">
+            <span>ul. Madalińskiego 8</span>
+            <span>70-101 Szczecin</span>
+          </div>
+        </div>
+      </div>
     </div>
+    <div class="d-flex flex-column align-items-start col-3 third-column">
+      <div class="footer-contacts-box d-flex flex-column">
+        <h2>Kontakt produkcja</h2>
+        <div class="footer-contact-box">
+          <div class="contact-icon">
+            <img src="./assets/icons/common/footer/blue-telephone.svg" alt="telefon" width="18" height="18">
+          </div>
+          <a class="gtm-phone" href="tel:91 433 97 11">91 433 97 11</a>
+        </div>
+        <div class="footer-contact-box">
+          <div class="contact-icon">
+            <img src="./assets/icons/common/footer/blue-location.svg" alt="adres" width="18" height="18">
+          </div>
+          <div class="address-footer d-flex flex-column">
+            <span>ul. Wrocławska 41 Byków</span>
+            <span>55-095 Mirków</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="d-flex flex-column align-items-start col-3 fourth-column">
+      <div class="footer-contacts-box d-flex flex-column">
+        <h2>Zapisz się do newslettera</h2>
+        <strong>Bądź z nami na bieżąco</strong>
+        <form class="newsletter-small-form" action="/newsletter/" method="post">
+          <div class="newsletter-small-form-box">
+            <input type="email" name="contact--newsletter_email" id="newsletter_input" class="email" placeholder="Twój adres e-mail">
+            <button id="newsletter_btn">Wyślij</button>
+          </div>
+          <input type="hidden" name="newsletter_send" value="1">
+          <label for="newsletter_terms1" class="nt1-label form-check-label custom-checkbox-container">
+            <input type="checkbox" name="newsletter_terms1" id="newsletter_terms1" value="1" class="newsletter-terms-1 form-check-label"> Zgoda na przetwarzanie danych osobowych Wyrażam zgodę na przetwarzanie moich danych osobowych przez [GiftSerwis] w celu otrzymywania newslettera zawierającego informacje o nowościach, promocjach i ofertach specjalnych. Przyjmuję do wiadomości, że mogę w każdej chwili wycofać zgodę, kontaktując się z administratorem danych lub klikając link rezygnacji w otrzymanej wiadomości. Szczegóły dotyczące przetwarzania danych znajdują się w Polityce Prywatności. <span class="custom-checkbox-checkmark bg-white"></span>
+          </label>
+        </form>
+      </div>
+    </div>
+  </div>
+  <div class="footer__container__bottom d-flex justify-content-center align-items-center">
+    <p>All rights reserved. Copyright by GIFTSERWIS <span></span>. </p>
+    <p class="contip">Realizacja <a href="https://www.contip.net/">
+        <img src="./assets/img/common/footer/contip-logo.png" height="24" alt="Contip logo">
+      </a>
+    </p>
+  </div>
 </div>
