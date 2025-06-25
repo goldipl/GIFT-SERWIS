c9e4b26aa88fcd666308e847c39670bed5ad34d1
goldipl <mgodlewski92@gmail.com>
2025-06-25 20:57:47 +0200

    styling ask product

--- components/contact/contact_content.php
+++ components/contact/contact_content.php
@@ -1,30 +1,43 @@
 <div class="contact-container container">
-  <form class="send-form">
-    <div class="send-form-left">
-      <h2>Napisz do Nas</h2>
-      <div class="input-slot">
-        <label for="name-surname">Imię i nazwisko *</label>
-        <input class="form-input" type="text" name="name-surname" id="name-surname" placeholder="Jan Nowak" required>
-      </div>
-      <div class="input-slot">
-        <label for="email">Email *</label>
-        <input class="form-input" type="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" name="email" id="email" placeholder="mail@gmail.com" required>
-      </div>
-      <div class="input-slot">
-        <label for="telephone">Telefon *</label>
-        <input class="form-input" type="text" name="telephone" id="telephone" placeholder="555 555 666" required>
-      </div>
+  <form class="send-form-box">
+    <h2>Napisz do Nas</h2>
+    <span class="d-flex mb-2">Zapytanie:</span>
+    <div class="input-slot">
+      <p>
+        <a class="ask-product-link" href="">
+          <strong>Długopis metalowy (1363944)</strong>
+        </a>
+      </p>
+      <a class="ask-product-link" href="">
+        <img src="./assets/img/product_card/product-card-01.jpg" height="200">
+      </a>
     </div>
-    <div class="send-form-right">
-      <div class="input-slot">
-        <label for="msgarea">Wiadomość</label>
-        <textarea class="form-textarea" name="msgarea" id="msgarea" placeholder="Wiadomość...."></textarea>
+    <div class="send-form">
+      <div class="send-form-left">
+        <div class="input-slot">
+          <label for="name-surname">Imię i nazwisko *</label>
+          <input class="form-input" type="text" name="name-surname" id="name-surname" placeholder="Jan Nowak" required>
+        </div>
+        <div class="input-slot">
+          <label for="email">Email *</label>
+          <input class="form-input" type="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" name="email" id="email" placeholder="mail@gmail.com" required>
+        </div>
+        <div class="input-slot">
+          <label for="telephone">Telefon *</label>
+          <input class="form-input" type="text" name="telephone" id="telephone" placeholder="555 555 666" required>
+        </div>
       </div>
-      <div class="checkbox-button-box">
-        <label class="custom-checkbox-container">Wyrażam zgodę na przetwarzanie danych osobowych wpisanych w formularzu w celu otrzymania odpowiedzi na moje pytanie zgodnie z Polityką Prywatności. <input type="checkbox" class="custom_input">
-          <span class="custom-checkbox-checkmark"></span>
-        </label>
-        <button class="green-button">Wyślij</button>
+      <div class="send-form-right">
+        <div class="input-slot">
+          <label for="msgarea">Wiadomość</label>
+          <textarea class="form-textarea" name="msgarea" id="msgarea" placeholder="Wiadomość...."></textarea>
+        </div>
+        <div class="checkbox-button-box">
+          <label class="custom-checkbox-container">Wyrażam zgodę na przetwarzanie danych osobowych wpisanych w formularzu w celu otrzymania odpowiedzi na moje pytanie zgodnie z Polityką Prywatności. <input type="checkbox" class="custom_input">
+            <span class="custom-checkbox-checkmark"></span>
+          </label>
+          <button class="green-button">Wyślij</button>
+        </div>
       </div>
     </div>
   </form>
@@ -98,98 +111,98 @@
   </div>
 </div>
 <div class="contact-container container">
-    <h2>Konto bankowe</h2>
-    <div class="bank-accounts">
-        <div class="bank-accounts-col">
-            <strong>Rachunek do wpłat krajowych (PLN):</strong>
-            <div class="copy-field">
-                <span class="account-code">PL49 1020 2892 2276 3005 0000 0000</span>
-                <img class="copy-icon" src="./assets/icons/contact/copy.svg" alt="Kopiuj" width="18" height="18">
-            </div>
-        </div>
-        <div class="bank-accounts-col">
-             <strong>Rachunek do wpłat krajowych (EUR):</strong>
-             <div class="copy-field">
-                <span class="account-code">96 2490 0005 0000 4600 9862 0829</span>
-               <img class="copy-icon" src="./assets/icons/contact/copy.svg" alt="Kopiuj" width="18" height="18">
-             </div>
-        </div>
+  <h2>Konto bankowe</h2>
+  <div class="bank-accounts">
+    <div class="bank-accounts-col">
+      <strong>Rachunek do wpłat krajowych (PLN):</strong>
+      <div class="copy-field">
+        <span class="account-code">PL49 1020 2892 2276 3005 0000 0000</span>
+        <img class="copy-icon" src="./assets/icons/contact/copy.svg" alt="Kopiuj" width="18" height="18">
+      </div>
     </div>
+    <div class="bank-accounts-col">
+      <strong>Rachunek do wpłat krajowych (EUR):</strong>
+      <div class="copy-field">
+        <span class="account-code">96 2490 0005 0000 4600 9862 0829</span>
+        <img class="copy-icon" src="./assets/icons/contact/copy.svg" alt="Kopiuj" width="18" height="18">
+      </div>
+    </div>
+  </div>
 </div>
 <div class="contact-container container">
-    <h2>Dział handlowy</h2>
-    <div class="contact-team">
-        <div class="contact-team-slot">
-            <div class="contact-team-slot__img">
-                <img class="person-img" src="./assets/img/contact/person.png" alt="Person" width="240" height="253">
-            </div>
-            <div class="contact-team-slot__data">
-                <strong>Piotr Milewski</strong>
-                <span>managing director</span>
-                <div class="icon-contact-box">
-                    <img class="contact-icon" src="./assets/icons/contact/telephone.svg" alt="Telefon" width="18" height="18">
-                    <a class="contact-text gtm-phone" href="tel:601 796 858">tel. 601 796 858</a>
-                </div>
-                <div class="icon-contact-box">
-                    <img class="contact-icon" src="./assets/icons/contact/envelope.svg" alt="Email" width="18" height="18">
-                    <a class="contact-text gtm-mail" href="mailto:piotr@giftserwis.com.pl">piotr@giftserwis.com.pl</a>
-                </div>
-            </div>
+  <h2>Dział handlowy</h2>
+  <div class="contact-team">
+    <div class="contact-team-slot">
+      <div class="contact-team-slot__img">
+        <img class="person-img" src="./assets/img/contact/person.png" alt="Person" width="240" height="253">
+      </div>
+      <div class="contact-team-slot__data">
+        <strong>Piotr Milewski</strong>
+        <span>managing director</span>
+        <div class="icon-contact-box">
+          <img class="contact-icon" src="./assets/icons/contact/telephone.svg" alt="Telefon" width="18" height="18">
+          <a class="contact-text gtm-phone" href="tel:601 796 858">tel. 601 796 858</a>
         </div>
-        <div class="contact-team-slot">
-            <div class="contact-team-slot__img">
-                <img class="person-img" src="./assets/img/contact/person.png" alt="Person" width="240" height="253">
-            </div>
-            <div class="contact-team-slot__data">
-                <strong>Anna Marciniak</strong>
-                <span>kierownik działu handlowego</span>
-                <div class="icon-contact-box">
-                    <img class="contact-icon" src="./assets/icons/contact/telephone.svg" alt="Telefon" width="18" height="18">
-                    <a class="contact-text gtm-phone" href="tel:605 390 956">tel. 605 390 956</a>
-                </div>
-                <div class="icon-contact-box">
-                    <img class="contact-icon" src="./assets/icons/contact/envelope.svg" alt="Email" width="18" height="18">
-                    <a class="contact-text gtm-mail" href="mailto:anna@giftserwis.com.pl">anna@giftserwis.com.pl</a>
-                </div>
-            </div>
+        <div class="icon-contact-box">
+          <img class="contact-icon" src="./assets/icons/contact/envelope.svg" alt="Email" width="18" height="18">
+          <a class="contact-text gtm-mail" href="mailto:piotr@giftserwis.com.pl">piotr@giftserwis.com.pl</a>
         </div>
-        <div class="contact-team-slot">
-            <div class="contact-team-slot__img">
-                <img class="person-img" src="./assets/img/contact/person.png" alt="Person" width="240" height="253">
-            </div>
-            <div class="contact-team-slot__data">
-                <strong>Karolina Byczko</strong>
-                <span>specjalista ds. sprzedaży</span>
-                <div class="icon-contact-box">
-                    <img class="contact-icon" src="./assets/icons/contact/telephone.svg" alt="Telefon" width="18" height="18">
-                    <a class="contact-text gtm-phone" href="tel:605 390 957">tel. 605 390 957</a>
-                </div>
-                <div class="icon-contact-box">
-                    <img class="contact-icon" src="./assets/icons/contact/envelope.svg" alt="Email" width="18" height="18">
-                    <a class="contact-text gtm-mail" href="mailto:k.byczko@giftserwis.com.pl">k.byczko@giftserwis.com.pl</a>
-                </div>
-            </div>
-        </div>
-        <div class="contact-team-slot">
-            <div class="contact-team-slot__img">
-                <img class="person-img" src="./assets/img/contact/person.png" alt="Person" width="240" height="253">
-            </div>
-            <div class="contact-team-slot__data">
-                <strong>Weronika Jucha</strong>
-                <span>specjalista ds. klienta</span>
-                <div class="icon-contact-box">
-                    <img class="contact-icon" src="./assets/icons/contact/telephone.svg" alt="Telefon" width="18" height="18">
-                    <a class="contact-text gtm-phone" href="tel:783 704 030">tel. 783 704 030</a>
-                </div>
-                <div class="icon-contact-box">
-                    <img class="contact-icon" src="./assets/icons/contact/envelope.svg" alt="Email" width="18" height="18">
-                    <a class="contact-text gtm-mail" href="mailto:wjucha@giftserwis.com.pl">wjucha@giftserwis.com.pl</a>
-                </div>
-            </div>
-        </div>
+      </div>
     </div>
+    <div class="contact-team-slot">
+      <div class="contact-team-slot__img">
+        <img class="person-img" src="./assets/img/contact/person.png" alt="Person" width="240" height="253">
+      </div>
+      <div class="contact-team-slot__data">
+        <strong>Anna Marciniak</strong>
+        <span>kierownik działu handlowego</span>
+        <div class="icon-contact-box">
+          <img class="contact-icon" src="./assets/icons/contact/telephone.svg" alt="Telefon" width="18" height="18">
+          <a class="contact-text gtm-phone" href="tel:605 390 956">tel. 605 390 956</a>
+        </div>
+        <div class="icon-contact-box">
+          <img class="contact-icon" src="./assets/icons/contact/envelope.svg" alt="Email" width="18" height="18">
+          <a class="contact-text gtm-mail" href="mailto:anna@giftserwis.com.pl">anna@giftserwis.com.pl</a>
+        </div>
+      </div>
+    </div>
+    <div class="contact-team-slot">
+      <div class="contact-team-slot__img">
+        <img class="person-img" src="./assets/img/contact/person.png" alt="Person" width="240" height="253">
+      </div>
+      <div class="contact-team-slot__data">
+        <strong>Karolina Byczko</strong>
+        <span>specjalista ds. sprzedaży</span>
+        <div class="icon-contact-box">
+          <img class="contact-icon" src="./assets/icons/contact/telephone.svg" alt="Telefon" width="18" height="18">
+          <a class="contact-text gtm-phone" href="tel:605 390 957">tel. 605 390 957</a>
+        </div>
+        <div class="icon-contact-box">
+          <img class="contact-icon" src="./assets/icons/contact/envelope.svg" alt="Email" width="18" height="18">
+          <a class="contact-text gtm-mail" href="mailto:k.byczko@giftserwis.com.pl">k.byczko@giftserwis.com.pl</a>
+        </div>
+      </div>
+    </div>
+    <div class="contact-team-slot">
+      <div class="contact-team-slot__img">
+        <img class="person-img" src="./assets/img/contact/person.png" alt="Person" width="240" height="253">
+      </div>
+      <div class="contact-team-slot__data">
+        <strong>Weronika Jucha</strong>
+        <span>specjalista ds. klienta</span>
+        <div class="icon-contact-box">
+          <img class="contact-icon" src="./assets/icons/contact/telephone.svg" alt="Telefon" width="18" height="18">
+          <a class="contact-text gtm-phone" href="tel:783 704 030">tel. 783 704 030</a>
+        </div>
+        <div class="icon-contact-box">
+          <img class="contact-icon" src="./assets/icons/contact/envelope.svg" alt="Email" width="18" height="18">
+          <a class="contact-text gtm-mail" href="mailto:wjucha@giftserwis.com.pl">wjucha@giftserwis.com.pl</a>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
 <div class="contact-banner container">
-    <img src="./assets/icons/common/footer/logo-footer.svg" alt="GiftSerwis" width="195" height="55">
-    <strong>Zapraszamy do współpracy</strong>
+  <img src="./assets/icons/common/footer/logo-footer.svg" alt="GiftSerwis" width="195" height="55">
+  <strong>Zapraszamy do współpracy</strong>
 </div>
