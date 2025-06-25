794962846691552a4ec4a67cddf59f04392804d2
goldipl <mgodlewski92@gmail.com>
2025-06-25 21:16:19 +0200

    add templates and styling branding page

---
+++ components/branding/branding_content.php
@@ -0,0 +1,87 @@
+<div id="branding">
+  <div class="section-about-us container">
+    <div class="section-about-us__text">
+      <h2>Tampodruk</h2>
+      <div class="title-red-line"></div>
+      <p>Technika druku polegająca na nakładaniu farby drukarskiej za pomocą miękkiego silikonowego stempla zwanego tamponem. Za pomocą tamponu o odpowiednim kształcie wykonywany jest nadruk na nierównych i nieregularnych powierzchniach.</p>
+      <p>Przez dobranie odpowiedniej farby możliwe jest drukowanie na podłożach takich jak tworzywa sztuczne, guma, szkło, metal itp.</p>
+      <p>Posiadamy maszyny jedno-, dwu- i czterokolorowe – dysponując takim parkiem maszynowym jesteśmy w stanie wykonać najbardziej skomplikowane formy znakowania.</p>
+    </div>
+    <div class="section-about-us__img">
+      <img src="./assets/img/branding/branding01.png" alt="Znakowanie" width="344" height="344">
+    </div>
+  </div>
+  <div class="section-about-us container">
+    <div class="section-about-us__text">
+      <h2>Sitodruk</h2>
+      <div class="title-red-line"></div>
+      <p>Formę drukową stanowi prostokątna rama, zwykle aluminiowa z napiętą na niej siatką z szablonem. Szablon tworzy utwardzona warstwa światłoczuła, nie przepuszczająca farby, stanowiąca negatywowy obraz drukowanego wzoru.</p>
+      <p>Podczas drukowania w maszynie płaskiej, farba drukowa jest  rozprowadzana na całej powierzchni siatki i przesuwającym się po niej raklem jest przeciskana przez wolne oczka siatki bezpośrednio na podłoże drukowe. Posiadamy stoły sitodrukowe w formatach od B2 do A0, sześciokolorową karuzelę czteropozycyjną, zespoły przesuszające oraz prasy termiczne.</p>
+    </div>
+    <div class="section-about-us__img">
+      <img src="./assets/img/branding/branding02.png" alt="Znakowanie" width="344" height="344">
+    </div>
+  </div>
+  <div class="section-about-us container">
+    <div class="section-about-us__text">
+      <h2>Kalka ceramiczna</h2>
+      <div class="title-red-line"></div>
+      <p>Technika nanoszenia wzoru odbywa się poprzez wypalanie kalki ceramicznej w piecu w temperaturze ok. 800°C, co pozwala uzyskać bardzo dużą trwałość wykonanego zdobienia.</p>
+      <p>Tak naniesiony wydruk odporny jest na  ścieranie, podgrzewanie w kuchenkach mikrofalowych, mycie mechaniczne i działanie chemicznych środków czyszczących. Dekoracje wykonane techniką wypału kalki ceramicznej dają długotrwałą wytrzymałość (do 1500 cykli) na mycie w zmywarkach. Dysponujemy komorowymi piecami firmy RHODE.</p>
+    </div>
+    <div class="section-about-us__img">
+      <img src="./assets/img/branding/branding03.png" alt="Znakowanie" width="344" height="344">
+    </div>
+  </div>
+  <div class="section-about-us container">
+    <div class="section-about-us__text">
+      <h2>Grawer laserowy</h2>
+      <div class="title-red-line"></div>
+      <p>Urządzenia umożliwiające trwałe znakowanie przedmiotów wiązką lasera. Daje nam ogromne możliwości tworzenia jednostkowych, niekonwencjonalnych upominków z personalizacją. Posiadane przez nas urządzenia pozwalają na grawerowanie w wielu materiałach, takich jak:</p>
+      <ul>
+        <li>drewno</li>
+        <li>laminaty grawerskie</li>
+        <li>szkło</li>
+        <li>skóra</li>
+        <li>guma do wytwarzania pieczątek</li>
+        <li>stal, aluminium</li>
+      </ul>
+      <p>Dysponujemy laserowymi urządzeniami typu CO2, oraz YAVG</p>
+    </div>
+    <div class="section-about-us__img">
+      <img src="./assets/img/branding/branding04.png" alt="Znakowanie" width="344" height="344">
+    </div>
+  </div>
+  <div class="section-about-us container">
+    <div class="section-about-us__text">
+      <h2>Tłoczenie</h2>
+      <div class="title-red-line"></div>
+      <p>Proces tworzenia w materiale tłoczonym trójwymiarowego wzoru poprzez ściśnięcie matrycą zazwyczaj w celu uwypuklenia elementu.</p>
+      <p>Tłoczenie odbywa się w temperaturze otoczenia lub tłocznik jest podgrzewany do temperatury kilkudziesięciu °C, dzięki czemu tłoczony materiał po ustąpieniu tłoka w mniejszym stopniu zmienia strukturę. Czasem używane są wyższe temperatury, by tłoczony materiał dodatkowo przebarwić (np. skórę). Dysponujemy maszyną do hotprintu o pow. znakowania do 1500 cm. kw.</p>
+    </div>
+    <div class="section-about-us__img">
+      <img src="./assets/img/branding/branding05.png" alt="Znakowanie" width="344" height="344">
+    </div>
+  </div>
+  <div class="section-about-us container">
+    <div class="section-about-us__text">
+      <h2>Druk cyfrowy</h2>
+      <div class="title-red-line"></div>
+      <p>Wykonujemy bezpośredni wielokolorowy nadruk cyfrowy na koszulkach za pomocą wyspecjalizowanej drukarki marki TEXJET Plus. Nadruki wykonywane są zarówno na koszulkach białych jak i kolorowych. Format zadruku do 42x60cm.</p>
+      <p>Dzięki zastosowaniu specjalnych farb do  tkanin możliwy jest nadruk na materiałach bawełnianych oraz mieszanych. Po wykonaniu nadruku farba utrwalana jest za pomocą prasy transferowej w temperaturze 165°C. Drukarka wyposażona jest w 4 kasety z kolorami CMYK oraz w 4 kasety z biała farbą.</p>
+      <p>Najważniejsze zalety drukarki TEXJET Plus:</p>
+      <ul>
+        <li>realizacja już od 1szt</li>
+        <li>doskonała jakość i trwałość znakowania</li>
+        <li>ekspresowy czas realizacji (12szt/h)</li>
+        <li>wydruk bezpośrednio z komputera</li>
+        <li>druk białego tła na kolorowych tkaninach</li>
+        <li>druk jest odporny na pranie</li>
+        <li>możliwość personalizacji</li>
+      </ul>
+    </div>
+    <div class="section-about-us__img">
+      <img src="./assets/img/branding/branding06.png" alt="Znakowanie" width="344" height="344">
+    </div>
+  </div>
+</div>
